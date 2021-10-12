d6a6fa889066e48a06552d6d1ceac4fb0c26bef5
abhijeet-cr7 <abhijeetdbz7@gmail.com>
2021-10-13 00:54:46 +0530

    added the music part inside the login and also created uplaod functionality

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-
+require("dotenv/config");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const multer = require("multer");
 const app = express();
+const AWS = require("aws-sdk");
+// const uuid = require("uuid/v5");
 const PORT = process.env.PORT || 9002;
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ID,
+  secretAccessKey: process.env.AWS_SECRET,
+});
+const createUUID = () => {
+  let dt = new Date().getTime();
+  const uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+    /[xy]/g,
+    function (c) {
+      const r = (dt + Math.random() * 16) % 16 | 0;
+      dt = Math.floor(dt / 16);
+      return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
+    }
+  );
+  return uuid;
+};
+
+const storage = multer.memoryStorage({
+  destination: function (req, file, callback) {
+    callback(null, "");
+  },
+});
+const upload = multer({ storage }).single("audio");
 
 mongoose.connect(
   "mongodb://localhost:27017/myLoginRegisterDB",
@@ -28,6 +55,26 @@ const userSchema = new mongoose.Schema({
 const User = new mongoose.model("User", userSchema);
 
 //Routes
+app.post("/upload", upload, (req, res) => {
+  let myFile = req.file.originalname.split(".");
+  const fileType = myFile[myFile.length - 1];
+
+  console.log(req.file);
+  // res.send({
+  //   message: "Hello World",
+  // });
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: `${createUUID()}.${fileType}`,
+    Body: req.file.buffer,
+  };
+  s3.upload(params, (error, data) => {
+    if (error) {
+      res.status(500).send(error);
+    }
+    res.status(200).send(data);
+  });
+});
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
   User.findOne({ email: email }, (err, user) => {
--- login-and-register-frontend/src/components/homepage/homepage.js
+++ login-and-register-frontend/src/components/homepage/homepage.js
@@ -1,13 +1,40 @@
-import React from "react"
-import "./homepage.css"
+import React from "react";
+import "./homepage.css";
+import Mainplayer from "./Mainplayer";
+import axios from "axios";
+import { useRef } from "react";
 
-const Homepage = ({setLoginUser}) => {
-    return (
-        <div className="homepage">
-            <h1>Hello Homepage</h1>
-            <div className="button" onClick={() => setLoginUser({})} >Logout</div>
-        </div>
-    )
-}
+const Homepage = ({ setLoginUser }) => {
+  const inputRef = useRef(null);
+  const upload = () => {
+    var formData = new FormData();
+    formData.append("audio", inputRef.files[0]);
+    axios.post("upload_file", formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+  };
+  return (
+    <div className="homepage">
+      {/* <h1>Hello Homepage</h1> */}
+      <Mainplayer />
+      <div
+        style={{
+          textAlign: "center",
+          marginTop: "10px",
+          fontSize: "20px",
+          color: "blue",
+          cursor: "pointer",
+        }}
+        onClick={() => setLoginUser({})}
+      >
+        <b>Logout</b>
+      </div>
+      <form enctype="multipart/form-data">
+        <input type="file" name="audio" ref={inputRef} />
+        <button onClick={upload}>UPLOAD</button>
+      </form>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
